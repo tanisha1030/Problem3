badda3b5c7d4109be7ada827159d37420e4fd1e8
tanisha1030 <149899016+tanisha1030@users.noreply.github.com>
2025-06-21 15:14:49 +0530

    Deleted train_cgan.py

--- train_cgan.py
+++ train_cgan.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-import numpy as np
+import numpy as np 
 import os
  
 # Generator
